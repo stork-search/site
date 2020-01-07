55363b8bd13a5307ac6ff3fb8fbf9a42b1ae3e9c
James Little <littleguy23@gmail.com>
2020-01-06 22:16:48 -0500

    Update stats

--- components/stats.vue
+++ components/stats.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="stats">
     <div class="flex">
-      <p>Latest version: <strong>0.2.0</strong></p>
+      <p>Latest version: <strong>0.3.0</strong></p>
       <p>Latest index protocol: <strong>1.0.0</strong></p>
-      <p>Javascript library last updated: <strong>2020-01-04</strong></p>
+      <p>Javascript library last updated: <strong>2020-01-06</strong></p>
     </div>
     <p class="github-cta">
       Check out the latest release
