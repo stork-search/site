ad6fb005c325f597656a438cfc4252d5122ea0a2
James Little <littleguy23@gmail.com>
2020-01-28 22:33:57 -0800

    Adjust topbar
    - Remove themes and integrations page
    - Add warning

--- components/topbar.vue
+++ components/topbar.vue
@@ -1,12 +1,17 @@
 <template>
-  <section class="top-bar">
-    <a href="/">Home</a>
-    <a href="/docs">Documentation</a>
-    <a href="/themes">Themes</a>
-    <a href="/integrations">Integrations</a>
-    <a href="/changelog">Changelog</a>
-    <a href="/roadmap">Roadmap</a>
-  </section>
+  <header>
+    <section class="warning">
+      Stork is in beta. Please adjust your expectations accordingly.
+    </section>
+    <section class="top-bar">
+      <a href="/">Home</a>
+      <a href="/docs">Documentation</a>
+      <!-- <a href="/themes">Themes</a>
+    <a href="/integrations">Integrations</a> -->
+      <a href="/changelog">Changelog</a>
+      <a href="/roadmap">Roadmap</a>
+    </section>
+  </header>
 </template>
 
 <style lang="scss" scoped>
@@ -16,6 +21,12 @@ section {
   background-color: $accent-color;
   padding-top: 1em;
   padding-bottom: 1em;
+}
+
+.warning {
+  text-align: center;
+  background-color: #e9e777;
+  font-weight: bold;
 }
 
 a {
