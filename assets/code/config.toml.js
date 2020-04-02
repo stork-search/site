4eb7388d3476c6f3d1a85b5b5e8993c4dd70f877
James Little <littleguy23@gmail.com>
2020-04-01 21:28:03 -0700

    Whoops

--- assets/code/config.toml.js
+++ assets/code/config.toml.js
@@ -1,4 +1,5 @@
-export default `[input]base_directory = "test/federalist"
+export default `[input]
+base_directory = "test/federalist"
 files = [
     {path = "federalist-1.txt", url = "/federalist-1/", title = "Introduction"},
     {path = "federalist-2.txt", url = "/federalist-2/", title = "Concerning Dangers from Foreign Force and Influence"},
