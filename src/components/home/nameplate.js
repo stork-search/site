d81d7fb3df50858a1a8f6d56406ea4116a21d95f
James Little <3058200+jameslittle230@users.noreply.github.com>
2021-04-28 19:14:58 -0700

    Add a logo (#15)
    * Add logo
    
    * Add logo attribution to the footer
    
    * Poke words
    
    * Logo thanks
    
    Co-authored-by: James Little <james@Jamess-Mac-mini.local>

--- src/components/home/nameplate.js
+++ src/components/home/nameplate.js
@@ -5,20 +5,34 @@ import { PageTitle, StylisticText } from '../text'
 const Intro = () => {
   return (
     <Column>
-      <PageTitle fontSize="4rem">Stork</PageTitle>
-      <StylisticText
-        style={{
-          fontSize: '1.8rem',
-          fontWeight: 'bold',
-          marginBottom: '0.4em',
-          letterSpacing: '-0.02em',
-        }}
-      >
-        Impossibly fast web search, made for static&nbsp;sites.
-      </StylisticText>
-      <StylisticText size="1.2rem" margin="0.6em 0">
-        By <a href="https://jameslittle.me">James Little</a>.
-      </StylisticText>
+      <div style={{ display: 'flex', alignItems: 'flex-end' }}>
+        <div
+          style={{
+            width: '13rem',
+            height: '13rem',
+            background: 'url(\'/logo.svg\') bottom right / 100% 100% no-repeat',
+            marginTop: '-4rem',
+            marginRight: '2rem',
+            flexShrink: 0
+          }}
+        ></div>
+        <div>
+          <PageTitle fontSize="4rem">Stork</PageTitle>
+          <StylisticText
+            style={{
+              fontSize: '1.8rem',
+              fontWeight: 'bold',
+              marginBottom: '0.4em',
+              letterSpacing: '-0.02em',
+            }}
+          >
+            Impossibly fast web search, made for static&nbsp;sites.
+          </StylisticText>
+          <StylisticText size="1.2rem" margin="0.6em 0">
+            By <a href="https://jameslittle.me">James Little</a>.
+          </StylisticText>
+        </div>
+      </div>
     </Column>
   )
 }
