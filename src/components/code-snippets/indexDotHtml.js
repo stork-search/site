3ad934844a9412006d95e574ae84425b919da6b1
James Little <littleguy23@gmail.com>
2021-06-05 21:14:55 -0700

    Replace codeblocks on home page

---
+++ src/components/code-snippets/indexDotHtml.js
@@ -0,0 +1,24 @@
+const indexDotHtml = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Federalist Search</title>
+    <link rel="stylesheet" href="https://files.stork-search.net/basic.css" />
+  </head>
+  <body>
+    <div class="stork-wrapper">
+      <input data-stork="federalist" class="stork-input" />
+      <div data-stork="federalist-output" class="stork-output"></div>
+    </div>
+      
+    <script src="https://files.stork-search.net/stork.js"></script>
+    <script>
+      stork.register(
+        'federalist',
+        'https://files.stork-search.net/federalist.st'
+      )
+    </script>
+  </body>
+</html>`
+
+export default indexDotHtml
