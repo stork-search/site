37bfe24987522ce4535997612fc53e3667c95999
James Little <littleguy23@gmail.com>
2021-12-01 22:28:33 -0500

    Add ID links to API reference pages

--- src/components/docs/api-reference.js
+++ src/components/docs/api-reference.js
@@ -1,17 +1,25 @@
 import ReactMarkdown from 'react-markdown'
 import styled from 'styled-components'
 
-const Option = styled.div`
-  padding: 0.7em 0;
+const Option = styled.div.attrs(({ id }) => ({ id }))`
+  padding: 0.7em 0.4em;
   margin: 0.7em 0;
   border-bottom: 1px solid hsla(0, 0%, 0%, 0.2);
+
+  &:target {
+    background: hsla(0, 0%, 0%, 0.1);
+  }
 `
 
-const KeyElement = styled.span`
+const KeyElementWrapper = styled.span`
   font-size: 1.4rem;
   font-family: var(--mono-font-family);
   font-weight: bold;
 `
+
+const KeyElement = ({ children }) => (
+  <KeyElementWrapper id={children}>{children}</KeyElementWrapper>
+)
 
 const Type = styled.span`
   color: hsla(0, 0%, 0%, 0.8);
@@ -34,13 +42,28 @@ const Default = styled.span`
   }
 `
 
+const IdLink = styled.a`
+  text-decoration: none;
+  margin-left: -1.2em;
+  margin-right: 0.5em;
+
+  &:link {
+    color: hsla(0, 0%, 0%, 20%);
+  }
+
+  &:hover {
+    color: hsla(0, 0%, 0%, 60%);
+  }
+`
+
 const Required = () => <RequiredWrapper>Required</RequiredWrapper>
 
 const ApiReferenceTable = ({ options }) => (
   <>
     {options.map((option) => (
-      <Option key={option.key}>
+      <Option id={option.key} key={option.key}>
         <p style={{ margin: 0 }}>
+          <IdLink href={`#${option.key}`}>#</IdLink>
           <KeyElement>{option.key}</KeyElement> •{' '}
           <Type>
             <ReactMarkdown
@@ -48,7 +71,7 @@ const ApiReferenceTable = ({ options }) => (
               unwrapDisallowed={true}
             >
               {option.type}
-            </ReactMarkdown>
+            </ReactMarkdown>{' '}
           </Type>
         </p>
         <p style={{ margin: 0, marginBottom: '0.45em', lineHeight: 1 }}>
