8878086d811bd5d7c05f06053871cd8b55c348b9
James Little <littleguy23@gmail.com>
2022-02-21 18:31:35 -0500

    Add sticker link

--- src/components/home/nameplate.js
+++ src/components/home/nameplate.js
@@ -1,14 +1,43 @@
 import styled from 'styled-components'
 import { Column, VerticalSpacer } from '../utils'
 import { PageTitle, StylisticText } from '../text'
+import Link from 'next/link'
 
-const Logo = styled.div`
+const Logo = () => (
+  <Link href="/sticker">
+    <InnerLogo />
+  </Link>
+)
+
+const InnerLogo = styled.div`
   width: 13rem;
   height: 13rem;
-  background: url("logo.svg") bottom right / 100% 100% no-repeat;
+  background: url('logo.svg') bottom right / 100% 100% no-repeat;
   margin-top: -4rem;
   margin-right: 2rem;
   flex-shrink: 0;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+
+  &:hover {
+    cursor: pointer;
+    -webkit-filter: drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.4));
+    filter: drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.4));
+
+    &:after {
+      content: 'Buy a sticker!';
+      display: block;
+      background: hsla(0, 0%, 98%, 1);
+      border: 1px solid #444;
+      position: bottom;
+      text-align: center;
+      font-size: 0.8rem;
+      padding: 0.2rem 0.6rem;
+      border-radius: 0.2rem;
+      margin-left: 2rem;
+    }
+  }
 
   @media (max-width: 40rem) {
     width: 10rem;
