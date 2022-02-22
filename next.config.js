d93ea41c436406d03a497af928333f3b7dfd7d8f
James Little <3058200+jameslittle230@users.noreply.github.com>
2022-02-21 23:30:22 -0500

    Add sticker link (#41)

--- next.config.js
+++ next.config.js
@@ -6,4 +6,14 @@ const withMDX = require('@next/mdx')({
 })
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx'],
+  async redirects() {
+    return [
+      {
+        source: '/sticker',
+        destination: 'https://buy.stripe.com/5kAdTm2MF8ejcIU4gg',
+        permanent: false,
+        basePath: false,
+      },
+    ]
+  },
 })
