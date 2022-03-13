ca91ed490fc67290de0cc4a183e7e96e344a1d99
James Little <littleguy23@gmail.com>
2022-03-12 20:22:20 -0500

    Update version to 1.4.1-rc.5

--- src/components/stork.js
+++ src/components/stork.js
@@ -1,18 +1,18 @@
 import { useEffect } from 'react'
 
 const indexUrls = {
-  federalist: 'https://files.stork-search.net/releases/v1.4.1-rc.4/federalist.st',
+  federalist: 'https://files.stork-search.net/releases/v1.4.1-rc.5/federalist.st',
   'federalist-basic':
-    'https://files.stork-search.net/releases/v1.4.1-rc.4/federalist.st',
+    'https://files.stork-search.net/releases/v1.4.1-rc.5/federalist.st',
   'federalist-dark':
-    'https://files.stork-search.net/releases/v1.4.1-rc.4/federalist.st',
+    'https://files.stork-search.net/releases/v1.4.1-rc.5/federalist.st',
   'federalist-flat':
-    'https://files.stork-search.net/releases/v1.4.1-rc.4/federalist.st',
+    'https://files.stork-search.net/releases/v1.4.1-rc.5/federalist.st',
   'federalist-edible':
-    'https://files.stork-search.net/releases/v1.4.1-rc.4/federalist.st',
+    'https://files.stork-search.net/releases/v1.4.1-rc.5/federalist.st',
   'federalist-edible-dark':
-    'https://files.stork-search.net/releases/v1.4.1-rc.4/federalist.st',
-  threeblue: 'https://files.stork-search.net/releases/v1.4.1-rc.4/3b1b.st',
+    'https://files.stork-search.net/releases/v1.4.1-rc.5/federalist.st',
+  threeblue: 'https://files.stork-search.net/releases/v1.4.1-rc.5/3b1b.st',
 }
 
 const Stork = ({
