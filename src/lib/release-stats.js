94b35b190bb2304918c9784d420979a80c40f71e
James Little <littleguy23@gmail.com>
2022-05-22 14:52:37 -0400

    Rebuild benchmark reports

--- src/lib/release-stats.js
+++ src/lib/release-stats.js
@@ -1,119 +1,59 @@
 export default [
+  // {
+  //   version: '1.5.0',
+  //   stats: {},
+  // },
+  {
+    version: '1.4.2',
+    stats: {
+      'stork.js': 21.88,
+      'stork.wasm': 354.761,
+      'federalist.st': 1125.456,
+      'build/federalist': 233.5403,
+      'search/federalist/liberty': 1.9579,
+    },
+  },
+  {
+    version: '1.4.1',
+    stats: {
+      'stork.js': 21.88,
+      'stork.wasm': 354.761,
+      'federalist.st': 1125.456,
+      'build/federalist': 244.6861,
+      'search/federalist/liberty': 1.9726,
+    },
+  },
+
+  {
+    version: '1.4.0',
+    stats: {
+      'stork.js': 21.88,
+      'stork.wasm': 316.386,
+      'federalist.st': 1125.456,
+      'build/federalist': 162.77009981999998,
+      'search/federalist/liberty': 2.2803661744963533,
+    },
+  },
   {
     version: '1.3.0',
-    jsSize: 21.677,
-    wasmSize: 351.12,
-    indexSize: 1125.456,
-    benchmarks: {
-      'build::federalist': 179.348,
-      'search::federalist::liberty': 2.167,
+    stats: {
+      'stork.js': 21.677,
+      'stork.wasm': 353.624,
+      'federalist.st': 1125.456,
+      'build/federalist': 165.8534055,
+      'search/federalist/liberty': 1.9696195653369153,
     },
   },
-  {
-    version: '1.2.1',
-    indexSize: 1125.456,
-    wasmSize: 518.59,
-    jsSize: 106.7,
-    benchmarks: {
-      'build::federalist': 151.06998776,
-      'search::federalist::liberty': 2.0661,
-    },
-  },
-  {
-    version: '1.2.0',
-    indexSize: 1125.45,
-    wasmSize: 518.52,
-    jsSize: 106.7,
-    // benchmarks: {
-    //   "build::federalist": 160.75,
-    //   "search::federalist::liberty": 2.5441
-    // }
-  },
-  {
-    version: '1.1.0',
-    indexSize: 1125.45,
-    wasmSize: 487.04,
-    jsSize: 106.87,
-    benchmarks: {
-      'build::federalist': 160.75,
-      'search::federalist::liberty': 2.5441,
-    },
-  },
-  {
-    version: '1.0.4',
-    indexSize: 1125.46,
-    wasmSize: 484.57,
-    jsSize: 92.24,
-    benchmarks: {
-      'build::federalist': 157.8,
-      'search::federalist::liberty': 2.4904,
-    },
-  },
-  {
-    version: '1.0.3',
-    indexSize: 1110.0,
-    wasmSize: 591.97,
-    jsSize: 94.75,
-  },
-  {
-    version: '1.0.2',
-    indexSize: 1110.0,
-    wasmSize: 591.97,
-    jsSize: 94.75,
-  },
-  {
-    version: '1.0.1',
-    indexSize: 1110.0,
-    wasmSize: 591.97,
-    jsSize: 94.75,
-  },
-  {
-    version: '1.0.0',
-    indexSize: 1110.0,
-    wasmSize: 591.97,
-    jsSize: 94.75,
-    duration: 0.367,
-  },
-  {
-    version: '0.7.4',
-    indexSize: 1110.0,
-    wasmSize: 578.84,
-    jsSize: 73.05,
-    duration: 0.354,
-  },
-  {
-    version: '0.7.3',
-    indexSize: 1140.0,
-    wasmSize: 578.36,
-    jsSize: 73.05,
-    duration: 0.363,
-  },
-  {
-    version: '0.7.2',
-    indexSize: 1140.0,
-    wasmSize: 578.36,
-    jsSize: 73.05,
-    duration: 0.353,
-  },
-  {
-    version: '0.7.1',
-    indexSize: 1140.0,
-    wasmSize: 674.7,
-    jsSize: 73.05,
-    duration: 0.365,
-  },
-  {
-    version: '0.7.0',
-    indexSize: 1140.0,
-    wasmSize: 684.82,
-    jsSize: 9.07,
-    duration: 0.383,
-  },
-  {
-    version: '0.6.0',
-    indexSize: 1800.0,
-    wasmSize: 191.37,
-    jsSize: 648.65,
-    duration: 0.218,
-  },
+  // {
+  //   version: '1.2.1',
+  //   stats: {},
+  // },
+  // {
+  //   version: '1.2.0',
+  //   stats: {},
+  // },
+  // {
+  //   version: '1.1.0',
+  //   stats: {},
+  // },
 ]
